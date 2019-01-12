bda9bd3394062ea68ae460e36288509ac5b80843
boschsu <bosch.su@hotmail.com>
2019-01-12 17:59:42 +0800

    Add mongodb

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
 process.title='Messenger'
 
+var url_db='mongodb://127.0.0.1:27017/messenger';
 var websocketPort=3001
 var websocketServer=require('websocket').server
 
-var history=[];
-var clients=[]
-
 function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+	return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
 
 var colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
 colors.sort(function(a,b) { return Math.random() > 0.5; } );
 
 
+var MongoClient=require('mongodb').MongoClient;
 var connect = require('connect');
 var http=require('http');
 var fs=require('fs');
 var path=require('path');
+var url=require('url');
 var requestIp=require('request-ip')
+var querystring=require('querystring')
 var app=connect();
 
 app.use(requestIp.mw({ attributeName : 'myCustomAttributeName' }))
-
-
 
 var server=http.createServer(
 	
@@ -32,36 +31,60 @@ var server=http.createServer(
 		function(request,response){
 
 			var ip = request.myCustomAttributeName;
-		    var fileName=path.basename(request.url) || 'index.html';
-		    var fullPath=__dirname+'/src/'+fileName;
-		    console.log('Request for '+fullPath+' received. Client ip is '+ip);
 
-			fs.readFile(fullPath,function(error,fileBinary){
-		        if (error){
-		            console.log(error);
-		            response.writeHead(200,{
-		                'Content-Type':'text/html'
-		            });
-		            response.write('no such file');
-		        }else {
-		            response.writeHead(200,{
-		                'Content-Type':'text/html'
-		            });
-		            response.write(fileBinary.toString());
-		        }
-		        response.end();
-		    });
-	    }
+			if (url.parse(request.url).pathname=='/postlogin'){
+				var _data='';
+				request.on('data',function(data){
+					_data+=data;
+				})
+				request.on('end',function(){
+					response.writeHead(200, {'Content-Type': 'application/json'});					
+					// var data={
+					// 	name:'高级'
+					// }
+					// response.end( JSON.stringify(_data) );
+					// var result=decodeURIComponent(_data)
+					var _result=querystring.parse(_data)
+					var result=JSON.stringify(_result)					
+					response.end(result)
+					insertOneToDB(_result)
+				})
+			}else {
+
+				var fileName=path.basename(request.url) || 'index.html';
+				var fullPath=__dirname+'/src/'+fileName;
+				console.log('Request for '+fullPath+' received. Client ip is '+ip);
+
+				fs.readFile(fullPath,function(error,fileBinary){
+					if (error){
+						console.log(error);
+						response.writeHead(200,{
+							'Content-Type':'text/html'
+						});
+						response.write('no such file');
+					}else {
+						response.writeHead(200,{
+							'Content-Type':'text/html'
+						});
+						response.write(fileBinary.toString());
+					}
+					response.end();
+				});
+
+			}
+		}
 	)
 )
 
 server.listen(websocketPort,function(){
-	console.log( (new Date())+" Server is listening on port "+websocketPort )
+	//console.log( (new Date())+" Server is listening on port "+websocketPort )
 })
 
 var ws=new websocketServer({
 	httpServer:server
 })
+var history=[];
+var clients=[]
 
 ws.on('request',function(request){
 	console.log((new Date())+" Connection from origin "+request.origin+'.');
@@ -72,14 +95,16 @@ ws.on('request',function(request){
 	var userName=false;
 	var userColor=false;
 
-	console.log((new Date()) + ' Connection accepted.');
+	//console.log((new Date()) + ' Connection accepted.');
 
-	if (history.length>0) {
-		connection.sendUTF(JSON.stringify({
-			type:'history',
-			data:history
-		}))
-	}
+	exportDialogueFromDB(function(result){
+		if (result.length>0) {
+			connection.sendUTF(JSON.stringify({
+				type:'history',
+				data:result
+			}))
+		}
+	})			
 
 	connection.on('message',function(message){
 		if (message.type==='utf8') {
@@ -88,7 +113,7 @@ ws.on('request',function(request){
 				userColor=colors.shift()
 				connection.sendUTF(JSON.stringify({
 					type:'color',
-					data:'userColor'
+					data:userColor
 				}))
 				console.log((new Date()) + ' User is known as: ' + userName + ' with ' + userColor + ' color.');
 			}else {
@@ -99,7 +124,7 @@ ws.on('request',function(request){
 					author:userName,
 					color:userColor
 				}
-				history.push(obj)
+				insertDialogueToDB(obj);
 				history=history.slice(-100)
 
 				var json=JSON.stringify({
@@ -107,20 +132,86 @@ ws.on('request',function(request){
 					data: obj 
 				});
 				for (var i=0;i<clients.length;i++){
-					clients[i].sendUTF(json)
+					clients[i].sendUTF(json)//boradcast message to all connections
 				}
 			}
 		}
 	})
 
 	connection.on('close', function(connection) {
-        if (userName !== false && userColor !== false) {
-            console.log((new Date()) + " Peer "
-                + connection.remoteAddress + " disconnected.");
-            // remove user from the list of connected clients
-            clients.splice(index, 1);
-            // push back user's color to be reused by another user
-            colors.push(userColor);
-        }
-    });
+		if (userName !== false && userColor !== false) {
+			console.log((new Date()) + " Peer "
+				+ connection.remoteAddress + " disconnected.");
+			// remove user from the list of connected clients
+			clients.splice(index, 1);
+			// push back user's color to be reused by another user
+			colors.push(userColor);
+		}
+	});
 })
+
+function insertOneToDB(entry){
+	MongoClient.connect(url_db,{ useNewUrlParser: true },function(error,mongo){
+		if (error) {
+			throw('step1:'+error)
+		}
+		var messengerDB=mongo.db('messenger')
+		var entryExist=false;
+
+		messengerDB.collection('conversations').find(entry,function(error,result){
+			if(error) {
+				throw('step2:'+error)
+			}
+			if (Object.is(JSON.stringify(result.cmd.query),JSON.stringify(entry))) {
+				entryExist=true;
+				mongo.close();
+				console.log('duplicate found, db closed.')
+			}			
+		})
+		if (entryExist==false) {
+			messengerDB.collection('conversations').insertOne(entry,function(error,result){
+				if(error) {
+					throw('step3:'+error)
+				}
+				console.log('Inserted one entry.')
+				mongo.close();
+			})	
+		}else {
+			mongo.close();
+		}
+	})
+}
+
+function insertDialogueToDB(entry){
+	MongoClient.connect(url_db,{ useNewUrlParser: true },function(error,mongo){
+		if (error) {
+			throw(error)
+		}
+		var messengerDB=mongo.db('messenger')	
+		
+		messengerDB.collection('dialogues').insertOne(entry,function(error,result){
+			if(error) {
+				throw(error)
+			}
+			console.log('Inserted one dialogue.')
+			mongo.close();
+		})			
+	})
+}
+
+function exportDialogueFromDB(next){
+	MongoClient.connect(url_db,{ useNewUrlParser: true },function(error,mongo){
+		if (error) {
+			throw(error)
+		}
+		var messengerDB=mongo.db('messenger')
+		
+		messengerDB.collection('dialogues').find({}).toArray(function(error,result){
+			if(error) {
+				throw(error)
+			}			
+			mongo.close();
+			next(result)
+		})
+	})
+}
