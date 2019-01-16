c205a099f9a7f2b821cc0530069878e32a52932c
boschsu <bosch.su@hotmail.com>
2019-01-16 18:31:31 +0800

    authentication and authorization added; register and logout added;

--- main.js
+++ main.js
@@ -14,15 +14,21 @@ colors.sort(function(a,b) { return Math.random() > 0.5; } );
 
 var MongoClient=require('mongodb').MongoClient;
 var connect = require('connect');
+var expressSession = require('express-session')
+var cookieParser = require('cookie-parser')
 var http=require('http');
 var fs=require('fs');
 var path=require('path');
 var url=require('url');
 var requestIp=require('request-ip')
 var querystring=require('querystring')
+var bcrypt=require('bcrypt')
 var app=connect();
 
-app.use(requestIp.mw({ attributeName : 'myCustomAttributeName' }))
+app.use(requestIp.mw({ attributeName : 'clientIP' }))
+app.use(cookieParser())
+var sessionParser=expressSession({ name: 'MessengerID', secret: 'node messenger', resave: false, saveUninitialized: true, cookie: { secure: false } })
+app.use(sessionParser)
 
 var server=http.createServer(
 	
@@ -30,31 +36,87 @@ var server=http.createServer(
 
 		function(request,response){
 
-			var ip = request.myCustomAttributeName;
-
+			var ip = request.clientIP;
+			var session=request.session;
+			//console.log(session)
 			if (url.parse(request.url).pathname=='/postlogin'){
 				var _data='';
 				request.on('data',function(data){
 					_data+=data;
 				})
 				request.on('end',function(){
-					response.writeHead(200, {'Content-Type': 'application/json'});					
+					response.writeHead(200, {'Content-Type': 'application/json'});
 					// var data={
 					// 	name:'高级'
 					// }
 					// response.end( JSON.stringify(_data) );
 					// var result=decodeURIComponent(_data)
 					var _result=querystring.parse(_data)
-					var result=JSON.stringify(_result)					
-					response.end(result)
-					insertOneToDB(_result)
+					var result=JSON.stringify(_result)
+					if (validationData(_result)) {
+						insertOneToDB(_result,function(result,options){
+							if (!result) {
+								response.end('');//wrong password
+							}else {
+								switch(options.type) {
+									case 'register':
+										session.username=_result.name
+										session.usercolor=userColor
+										response.end('{"name":"'+result.name+'"}')
+										break;
+									case 'login':
+										session.username=_result.name
+										session.usercolor=userColor
+										response.end('{"name":"'+_result.name+'"}')									
+										break;
+									default: 										
+										break;	
+								}
+								
+							}
+						})
+					}else {
+						console.log('POST wrong data.')
+						return false;
+					}
 				})
+			}else if(url.parse(request.url).pathname=='/getlogout'){
+				//console.log(request.query)
+				userName=false;
+				userColor=false;
+				session=null;
+				// console.log(session)
+				// session.destroy(function(error){
+				// 	console.log("SESSION DESTORY ERROR:",error)
+				// })
+				response.writeHead(200,{
+					'Content-Type':'text/html'
+				});
+				response.end('success');
 			}else {
-
 				var fileName=path.basename(request.url) || 'index.html';
 				var fullPath=__dirname+'/src/'+fileName;
-				console.log('Request for '+fullPath+' received. Client ip is '+ip);
-
+				console.log('Request for '+fullPath+' received.\r\n^^^Client ip is '+ip);
+				//console.log(session)
+				if (!session.username) {
+					//session.username='humor'
+				}else {
+					// console.log('Session Username: ',session.username)
+					// console.log('Session Usercolor: ',session.usercolor)
+					userName=session.username
+					userColor=session.usercolor
+					exportDialogueFromDB(function(result){
+						if (result.length>0) {
+							connection.sendUTF(JSON.stringify({
+								type:'history',
+								data:result,
+								name:userName,
+								color:userColor
+							}))
+						}
+					})
+				}
+				// console.log('Session Username : ',session.username)
 				fs.readFile(fullPath,function(error,fileBinary){
 					if (error){
 						console.log(error);
@@ -70,14 +132,13 @@ var server=http.createServer(
 					}
 					response.end();
 				});
-
 			}
 		}
 	)
 )
 
 server.listen(websocketPort,function(){
-	//console.log( (new Date())+" Server is listening on port "+websocketPort )
+	console.log("Server is listening on port "+websocketPort )
 })
 
 var ws=new websocketServer({
@@ -85,43 +146,48 @@ var ws=new websocketServer({
 })
 var history=[];
 var clients=[]
+var connection;
+var userName=false;
+var userColor=false;
 
 ws.on('request',function(request){
-	console.log((new Date())+" Connection from origin "+request.origin+'.');
-
-	var connection=request.accept(null,request.origin)
+	connection=request.accept(null,request.origin)
+	console.log("Websocket connection from "+request.origin+' accepted.');
 
 	var index=clients.push(connection)-1
-	var userName=false;
-	var userColor=false;
-
-	//console.log((new Date()) + ' Connection accepted.');
-
-	exportDialogueFromDB(function(result){
-		if (result.length>0) {
-			connection.sendUTF(JSON.stringify({
-				type:'history',
-				data:result
-			}))
-		}
-	})			
 
 	connection.on('message',function(message){
 		if (message.type==='utf8') {
 			if (userName===false) {
 				userName=htmlEntities(message.utf8Data);
 				userColor=colors.shift()
+				sessionParser(request.httpRequest, {}, function(){
+					request.httpRequest.session.usercolor=userColor
+			        request.httpRequest.session.save(function(error){
+			        	if (error) {
+			        		console.log("SESSION SAVE ERROR:",error)
+			        	}
+			        })
+			    });
 				connection.sendUTF(JSON.stringify({
 					type:'color',
 					data:userColor
-				}))
-				console.log((new Date()) + ' User is known as: ' + userName + ' with ' + userColor + ' color.');
+				}))				
+				console.log('session usercolor created')
+				exportDialogueFromDB(function(result){
+					if (result.length>0) {
+						connection.sendUTF(JSON.stringify({
+							type:'history',
+							data:result
+						}))
+					}
+				})
 			}else {
 				console.log((new Date()) + ' Received Message from ' + userName + ': ' + message.utf8Data);
 				var obj={
 					time:(new Date()).getTime(),
-					text:htmlEntities(message.utf8Data),
 					author:userName,
+					text:htmlEntities(message.utf8Data),					
 					color:userColor
 				}
 				insertDialogueToDB(obj);
@@ -150,36 +216,55 @@ ws.on('request',function(request){
 	});
 })
 
-function insertOneToDB(entry){
+function insertOneToDB(entry,callback){
 	MongoClient.connect(url_db,{ useNewUrlParser: true },function(error,mongo){
 		if (error) {
 			throw('step1:'+error)
 		}
 		var messengerDB=mongo.db('messenger')
-		var entryExist=false;
+		// var entryExist=false;
 
-		messengerDB.collection('conversations').find(entry,function(error,result){
-			if(error) {
-				throw('step2:'+error)
-			}
-			if (Object.is(JSON.stringify(result.cmd.query),JSON.stringify(entry))) {
-				entryExist=true;
-				mongo.close();
-				console.log('duplicate found, db closed.')
-			}			
-		})
-		if (entryExist==false) {
-			messengerDB.collection('conversations').insertOne(entry,function(error,result){
-				if(error) {
-					throw('step3:'+error)
+		messengerDB.collection('conversations').findOne(
+			{
+				name:entry.name
+			},
+			function(error,result){
+				if (!result) {
+					callback(registerFunc(mongo,messengerDB,entry),{type:'register'})
+				}else {
+					bcrypt.compare(entry.password,result.password,function(error,result){
+						if (result==true){
+							console.log('Welcome '+entry.name)
+							mongo.close();
+						}else {
+							console.log('Password invalid.')
+							mongo.close();
+						}
+						callback(result,{type:'login'})
+					})
 				}
-				console.log('Inserted one entry.')
-				mongo.close();
-			})	
-		}else {
-			mongo.close();
-		}
+				
+			})
 	})
+}
+
+function registerFunc(database,collection,entry){
+	var mongo=database
+	var messengerDB=collection
+	bcrypt.hash(entry.password,10,function(error,hash){
+		if(error){
+			throw('bcrypt error: '+error)
+		}
+		entry.password=hash;
+		messengerDB.collection('conversations').insertOne(entry,function(error,result){
+			if(error) {
+				throw('step3:'+error)
+			}			
+			console.log(entry.name+' registered, welcome!')
+			mongo.close();
+		})
+	})
+	return entry;
 }
 
 function insertDialogueToDB(entry){
@@ -214,4 +299,12 @@ function exportDialogueFromDB(next){
 			next(result)
 		})
 	})
+}
+
+function validationData(data) {
+	if (data.name==='') {
+		return false;
+	}else {
+		return true
+	}
 }
